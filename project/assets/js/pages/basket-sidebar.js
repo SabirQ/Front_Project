596a83ddf2a3d30b7225bb74f251ce4b211674a0
SabirQ <sabirmg@code.edu.az>
2022-05-29 20:21:24 +0400

    Basket bugs fixed

--- project/assets/js/pages/basket-sidebar.js
+++ project/assets/js/pages/basket-sidebar.js
@@ -25,3 +25,72 @@ cartSidebarCloseBtn.onclick=function(){
 // basket////////////////////////////////////////////////////
 
 
+const subtotalSideDiv=document.querySelector("._cart-sidebar__container__total-box__prices__sub-total");
+const ecoSideDiv=document.querySelector("._cart-sidebar__container__total-box__prices__eco-tax");
+const totalSideDiv=document.querySelector("._cart-sidebar__container__total-box__prices__total");
+const vatSideDiv=document.querySelector("._cart-sidebar__container__total-box__prices__vat");
+
+
+const itemParserSidebar = () =>
+  localStorage.getItem("items")
+    ? JSON.parse(localStorage.getItem("items"))
+    : [];
+
+const sidebar=document.querySelector("._cart-sidebar__container__products");
+const renderContentSidebar = () => {
+  const items = itemParserSidebar();
+  if (items.length > 0) {
+    sidebar.innerHTML = "";
+    let ecoside=items.length *5;
+      let subtotalrowside=0;
+     let shipside=0;
+    let totalside = 0;
+    let subtotalside=0;
+    items.forEach((item) => {
+      subtotalrowside=item.count*item.item.price;
+      subtotalside+=subtotalrowside;
+      shipside=subtotalside/100*20;
+      totalside=subtotalside+ecoside+shipside;
+      subtotalSideDiv.innerHTML="$"+subtotalside;
+      ecoSideDiv.innerHTML="$"+ecoside;
+      totalSideDiv.innerHTML="$"+totalside;
+      vatSideDiv.innerHTML="$"+shipside;
+
+
+
+sidebar.insertAdjacentHTML('afterbegin',` <div class="_cart-sidebar__container__products__selected">
+<a href="single-product.html" class="_cart-sidebar__container__products__selected__image">
+    <img src="${item.item.img}" alt="product" class="_cart-sidebar__container__products__selected__image__img">
+</a>
+<div class="_cart-sidebar__container__products__selected__info">
+    <a  href="single-product.html" class="_cart-sidebar__container__products__selected__info__title">
+        ${item.item.title}
+    </a>
+    <div class="_cart-sidebar__container__products__selected__info__price">
+        <span class="_cart-sidebar__container__products__selected__info__price__count">${item.count}</span>
+        <span class="_cart-sidebar__container__products__selected__info__price__static">
+            <i class="fa-solid fa-xmark"></i>
+        </span>
+        <span class="_cart-sidebar__container__products__selected__info__price__value">$${item.item.price}</span>
+    </div>
+</div>
+<button class="_cart-sidebar__container__products__selected__delete">
+    <i class="fa-solid fa-xmark"></i>
+</button>
+</div>`)
+
+    //   totalPrice += price * count;
+
+    //   renderCounter(divItem, id, count);
+    });
+
+    // totalPriceContainer.innerHTML = `${totalPrice} AZN`;
+  } else {
+    sidebar.innerHTML = "Your basket is empty";
+    // totalPriceContainer.innerHTML = ``;
+  }
+};
+
+renderContentSidebar();
+
+window.addEventListener("storage", renderContentSidebar);
--- project/assets/js/pages/basket.js
+++ project/assets/js/pages/basket.js
@@ -3,127 +3,141 @@ const container = document.querySelector(".cart__container__row__column__cus-tab
 const total = document.querySelector("._pro-total");
 const subtotal = document.querySelector("._pro-sub-total");
 
-// const total = document.querySelector("._pro-total");
-// const subtotal = document.querySelector("._pro-sub-total");
 
 const itemParser = () =>
   localStorage.getItem("items")
     ? JSON.parse(localStorage.getItem("items"))
     : [];
 
-// const decreaseItemCount = (id) => {
-//   let items = itemParser();
+const decreaseItemCount = (id) => {
+  let items = itemParser();
 
-//   items = items
-//     .map((item) => {
-//       if (item.item.id === id) {
-//         if (item.count > 1) {
-//           return {
-//             ...item,
-//             count: item.count - 1,
-//           };
-//         }
-//       } else {
-//         return item;
-//       }
-//     })
-//     .filter(Boolean);
+  items = items
+    .map((item) => {
+      if (item.item.id === id) {
+        if (item.count > 1) {
+          return {
+            ...item,
+            count: item.count - 1,
+          };
+        }
+      } else {
+        return item;
+      }
+    })
+    // .filter(Boolean);
 
-//   localStorage.setItem("items", JSON.stringify(items));
+  localStorage.setItem("items", JSON.stringify(items));
 
-//   renderContent();
-// };
+  renderContent();
+};
 
-// const increaseItemCount = (id) => {
-//   let items = itemParser();
+const increaseItemCount = (id) => {
+  let items = itemParser();
 
-//   items = items.map((item) => {
-//     if (item.item.id === id) {
-//       if (item.count < 10) {
-//         return {
-//           ...item,
-//           count: item.count + 1,
-//         };
-//       }
-//     }
+  items = items.map((item) => {
+    if (item.item.id === id) {
+      if (item.count < 10) {
+        return {
+          ...item,
+          count: item.count + 1,
+        };
+      }
+    }
 
-//     return item;
-//   });
+    return item;
+  });
 
-//   localStorage.setItem("items", JSON.stringify(items));
+  localStorage.setItem("items", JSON.stringify(items));
 
-//   renderContent();
-// };
+  renderContent();
+};
 
-    
-    //     const renderCounter = (parent,id, count) => {
-    //         const parentdiv=document.querySelectorAll(`.${parent}`)
-    //         parentdiv.insertAdjacentHTML('afterend', `<div class="plus-minus">
-    //         <button class="plus-minus__minus">-</button>
-    //         <input class="plus-minus__value" type="text" value="1">
-    //         <button class="plus-minus__plus">+</button>
-    //     </div>`)
-    //     // const minus=querySelectorAll(".plus-minus__minus")
-    //     // const value=querySelectorAll(".plus-minus__value")
-    //     // const plus=querySelectorAll(".plus-minus__plus")
-    //     // const proRow=querySelectorAll(".cart__container__row__column__cus-table__table__body__row")
+
+
+
+// const renderCounter = (id, count) => {
+//             const parentdiv=document.querySelectorAll(`.${parent}`)
+//             parentdiv.insertAdjacentHTML('afterend', `<div class="plus-minus">
+//             <button class="plus-minus__minus">-</button>
+//             <input class="plus-minus__value" type="text" value="1">
+//             <button class="plus-minus__plus">+</button>
+//         </div>`)
+       
+       
       
-    //     for ( let i = 0;i<minus.length;i++){
-    //       minus[i].addEventListener("click", () => {
-    //           decreaseItemCount(id);
-    //         });
-    //         plus[i].addEventListener("click", () => {
-    //           increaseItemCount(id);
-    //         });
-    //     }
-    //     value.innerHTML = count;
-    //   };
-  
-
+//         for ( let i = 0;i<minus.length;i++){
+//           minus[i].addEventListener("click", () => {
+//               decreaseItemCount(id);
+//             });
+//             plus[i].addEventListener("click", () => {
+//               increaseItemCount(id);
+//             });
+//         }
+//         value.innerHTML = count;
+//       };
+const subtotalDiv=document.querySelector("._pro-sub-total");
+const shippingDiv=document.querySelector("._pro-shipping");
+const totalDiv=document.querySelector("._pro-total");
 const renderContent = () => {
   const items = itemParser();
-
   if (items.length > 0) {
-    let subPrice = 0;
-    let totalPrice = 0;
-    container.insertAdjacentHTML('afterend', ``);
+    let eco=items.length *5;
+      let subtotalrow=0;
+     let subtotal=0;
+     let ship=0;
+    let total = 0;
+    // let eco=0;
+    container.innerHTML = "";
+    // sidebar.innerHTML = "";
+    items.forEach(item => {
+        
+        subtotalrow=item.count*item.item.price;
+        subtotal+=subtotalrow;
+        ship=(subtotal/100*20)+eco;
+        shippingDiv.innerHTML="$"+ship;
+        subtotalDiv.innerHTML="$"+subtotal;
+        total=subtotal+ship;
+        totalDiv.innerHTML="$"+total;
 
-    items.forEach(({ count, item: { img, title, id, price } }) => {
+ container.insertAdjacentHTML('afterbegin',` <tr class="cart__container__row__column__cus-table__table__body__row">
+ <td class="cart__container__row__column__cus-table__table__body__row__link">
+     <a href="single-product.html" class="cart__container__row__column__cus-table__table__body__row__link__image">
+         <img class="cart__container__row__column__cus-table__table__body__row__link__image__img" src="${item.item.img}" alt="product"/>
+     </a>
+ </td>
+ <td class="cart__container__row__column__cus-table__table__body__row__title">
+     <a href="single-product.html" class="cart__container__row__column__cus-table__table__body__row__title__link">
+         <span class="cart__container__row__column__cus-table__table__body__row__title__link__name">${item.item.title}</span>
+     </a>
+ </td>
+ <td class="cart__container__row__column__cus-table__table__body__row__price">
+     <span class="cart__container__row__column__cus-table__table__body__row__price__value">
+         $${item.item.price}
+     </span>
+ </td>
+ <td class="cart__container__row__column__cus-table__table__body__row__count">
+     <div class="plus-minus">
+         <button class="plus-minus__minus" onclick="decreaseItemCount('${item.item.id}')">-</button>
+         <input class="plus-minus__value" type="text" value="${item.count}">
+         <button class="plus-minus__plus" onclick="increaseItemCount('${item.item.id}')">+</button>
+     </div>
+ </td>
+ <td class="cart__container__row__column__cus-table__table__body__row__sub-total">
+     <span class="cart__container__row__column__cus-table__table__body__row__sub-total__value">
+         $${subtotalrow}
+     </span>
+ </td>
+ <td class="cart__container__row__column__cus-table__table__body__row__delete">
+     <button class="cart__container__row__column__cus-table__table__body__row__delete__btn">
+         <i class="fa-regular fa-trash-can"></i>
+     </button>
+ </td>
+</tr>`);
 
     //   totalPrice += price * count;
-      subPrice += price * count;
-      container.insertAdjacentHTML('afterend', `<tr class="cart__container__row__column__cus-table__table__body__row" id="${id}">
-      <td class="cart__container__row__column__cus-table__table__body__row__link">
-          <a href="single-product.html" class="cart__container__row__column__cus-table__table__body__row__link__image">
-              <img class="cart__container__row__column__cus-table__table__body__row__link__image__img" src="${img}" alt="product"/>
-          </a>
-      </td>
-      <td class="cart__container__row__column__cus-table__table__body__row__title">
-          <a href="single-product.html" class="cart__container__row__column__cus-table__table__body__row__title__link">
-              <span class="cart__container__row__column__cus-table__table__body__row__title__link__name">${title}</span>
-          </a>
-      </td>
-      <td class="cart__container__row__column__cus-table__table__body__row__price">
-          <span class="cart__container__row__column__cus-table__table__body__row__price__value">
-              $${price}
-          </span>
-      </td>
-      <td class="cart__container__row__column__cus-table__table__body__row__count">
-      
-      </td>
-      <td class="cart__container__row__column__cus-table__table__body__row__sub-total">
-          <span class="cart__container__row__column__cus-table__table__body__row__sub-total__value">
-              $${subPrice}
-          </span>
-      </td>
-      <td class="cart__container__row__column__cus-table__table__body__row__delete">
-          <button class="cart__container__row__column__cus-table__table__body__row__delete__btn">
-              <i class="fa-regular fa-trash-can"></i>
-          </button>
-      </td>
-  </tr>`);
 
-    //   renderCounter(cart__container__row__column__cus-table__table__body__row__count, id, count);
+    //   renderCounter(divItem, id, count);
     });
 
     // totalPriceContainer.innerHTML = `${totalPrice} AZN`;
@@ -133,6 +147,65 @@ const renderContent = () => {
   }
 };
 
-// renderContent();
+renderContent();
 
 window.addEventListener("storage", renderContent);
+
+  
+
+// const renderContent = () => {
+//   const items = itemParser();
+
+//   if (items.length > 0) {
+//     let subPrice = 0;
+//     let totalPrice = 0;
+//     container.insertAdjacentHTML('afterend', ``);
+
+//     items.forEach(({ count, item: { img, title, id, price } }) => {
+
+//       totalPrice += price * count;
+//       subPrice += price * count;
+//       container.insertAdjacentHTML('afterend', `<tr class="cart__container__row__column__cus-table__table__body__row" id="${id}">
+//       <td class="cart__container__row__column__cus-table__table__body__row__link">
+//           <a href="single-product.html" class="cart__container__row__column__cus-table__table__body__row__link__image">
+//               <img class="cart__container__row__column__cus-table__table__body__row__link__image__img" src="${img}" alt="product"/>
+//           </a>
+//       </td>
+//       <td class="cart__container__row__column__cus-table__table__body__row__title">
+//           <a href="single-product.html" class="cart__container__row__column__cus-table__table__body__row__title__link">
+//               <span class="cart__container__row__column__cus-table__table__body__row__title__link__name">${title}</span>
+//           </a>
+//       </td>
+//       <td class="cart__container__row__column__cus-table__table__body__row__price">
+//           <span class="cart__container__row__column__cus-table__table__body__row__price__value">
+//               $${price}
+//           </span>
+//       </td>
+//       <td class="cart__container__row__column__cus-table__table__body__row__count">
+      
+//       </td>
+//       <td class="cart__container__row__column__cus-table__table__body__row__sub-total">
+//           <span class="cart__container__row__column__cus-table__table__body__row__sub-total__value">
+//               $${subPrice}
+//           </span>
+//       </td>
+//       <td class="cart__container__row__column__cus-table__table__body__row__delete">
+//           <button class="cart__container__row__column__cus-table__table__body__row__delete__btn">
+//               <i class="fa-regular fa-trash-can"></i>
+//           </button>
+//       </td>
+//   </tr>`);
+
+//       renderCounter(cart__container__row__column__cus-table__table__body__row__count, id, count);
+//     });
+
+//     totalPriceContainer.innerHTML = `${totalPrice} AZN`;
+//   } else {
+//     container.innerHTML = "Your basket is empty";
+//     totalPriceContainer.innerHTML = ``;
+//   }
+// };
+
+// renderContent();
+
+// window.addEventListener("storage", renderContent);
